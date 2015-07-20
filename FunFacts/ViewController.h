999486279f854265dd94c7232ab8e2b9718ae877
Chloe <Chloe@Chloes-MBP.phub.net.cable.rogers.com>
2015-07-20 17:54:34 -0400

    Initial Commit

---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Chloe on 2015-07-20.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FunFacts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FunFacts
+//
+//  Created by Chloe on 2015-07-20.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
